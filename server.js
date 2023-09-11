b97fef6cf1f2d1df35972ba3bdf8b306027dcae3
jgunn <j.s.gunn22@gmail.com>
2023-09-11 13:17:31 -0700

    creates starting files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const routes = require("./routes");
+
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express().json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}!`);
+  });
+});
