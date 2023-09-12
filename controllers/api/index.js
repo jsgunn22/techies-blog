fe2d3fb7d5986b962fd0b58703c9401183246de9
jgunn <j.s.gunn22@gmail.com>
2023-09-12 13:38:30 -0700

    adds express-handlebars and handlebars

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+//
+// need to require routes from sibling files here
+//
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express().json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
